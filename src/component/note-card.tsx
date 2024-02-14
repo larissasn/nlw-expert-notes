d07eb7489627cfea130b92b45bf6ce7753a53263
Larissa Santos <106403123+larissasn@users.noreply.github.com>
2024-02-14 10:40:53 -0300

    nlw-experts-notes-pt.1

---
+++ src/component/note-card.tsx
@@ -0,0 +1,20 @@
+interface NoteCardProps {
+  note:{
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({note}: NoteCardProps) {
+  return (
+    <button className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2  hover:ring-slate-600 focus:ring-2 focus:ring-lime-500'>
+      <span className="text-sm leading-6 text-slate-400">
+        {note.date.toDateString()}
+      </span>
+      <p className='text-sm leading-6 text-slate-400'>
+        {note.content}
+      </p>
+      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+    </button>
+  )
+}
